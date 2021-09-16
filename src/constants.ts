2685cb6015a8fec4b1599a96b005947a3bc80466
Axel Froborg <23397446+afroborg@users.noreply.github.com>
2021-09-16 18:00:20 +0200

    test

--- src/constants.ts
+++ src/constants.ts
@@ -1,21 +1,23 @@
-import FiGithub from 'svelte-icons-pack/fi/FiGithub';
-import FiGitlab from 'svelte-icons-pack/fi/FiGitlab';
-import FiLinkedIn from 'svelte-icons-pack/fi/FiLinkedIn';
+// import FiGithub from 'svelte-icons-pack/fi/FiGithub';
+// import FiGitlab from 'svelte-icons-pack/fi/FiGitlab';
+// import FiLinkedIn from 'svelte-icons-pack/fi/FiLinkedIn';
 
-export const socials = [
-  {
-    title: 'Github',
-    icon: FiGithub,
-    href: 'http://github.com/afroborg',
-  },
-  {
-    title: 'GitLab',
-    icon: FiGitlab,
-    href: 'https://gitlab.com/afroborg',
-  },
-  {
-    title: 'LinkedIn',
-    icon: FiLinkedIn,
-    href: 'https://www.linkedin.com/in/axelfroborg',
-  },
-];
+// export const socials = [
+//   {
+//     title: 'Github',
+//     icon: FiGithub,
+//     href: 'http://github.com/afroborg',
+//   },
+//   {
+//     title: 'GitLab',
+//     icon: FiGitlab,
+//     href: 'https://gitlab.com/afroborg',
+//   },
+//   {
+//     title: 'LinkedIn',
+//     icon: FiLinkedIn,
+//     href: 'https://www.linkedin.com/in/axelfroborg',
+//   },
+// ];
+
+export const socials = [];
