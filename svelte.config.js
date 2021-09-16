4d74d09359ca3e29540d39df79caedaad3070f0f
Axel Froborg <23397446+afroborg@users.noreply.github.com>
2021-09-16 17:42:21 +0200

    add vercel support

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,25 @@
+import vercel from '@sveltejs/adapter-vercel';
 import path from 'path';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		vite: {
-			resolve: {
-				alias: {
-					'@': path.resolve('./src')
-				}
-			}
-		}
-	}
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+    vite: {
+      resolve: {
+        alias: {
+          '@': path.resolve('./src'),
+        },
+      },
+    },
+    adapter: vercel(),
+  },
 };
 
 export default config;
